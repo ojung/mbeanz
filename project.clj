4afda172113cb8ad80cc2326cb8c8ab99c2401f6
Oskar Jung <oskar@mbr-targeting.com>
2015-09-10 09:45:18 +1200

    Implement 3 methods to work with mbeans
    * describe: get description of a mbean
    * list: list all Adscale:* mbeans
    * params: get the signature of a mbean
    * invoke: execute a mbean

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "helper to find and use mbeans"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.jmx "0.3.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot mbeanz.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mbeanz/core.clj
+++ src/mbeanz/core.clj
@@ -1,29 +1,77 @@
 (ns mbeanz.core
   (:gen-class)
-  (:import (java.lang System)
-           (javax.management ObjectName
-                             MBeanOperationInfo)
-           (javax.management.remote JMXServiceURL
-                                    JMXConnectorFactory)))
+  (:require [clojure.java.jmx :as jmx]
+            [clojure.tools.cli :as cli]
+            [clojure.core.match :refer [match]]
+            [clojure.string :as string])
+  (:import (java.lang System)))
 
-(def jmx-url (JMXServiceURL. "service:jmx:rmi:///jndi/rmi://localhost:61342/jmxrmi"))
+(def object-name-pattern "Adscale:*")
 
-(def object-name-pattern (ObjectName. "*:*"))
+(def cli-options [["-m" "--mode list" "mode"
+                   :id :mode
+                   :parse-fn #(keyword %)]
+                  ["-b" "--bean some-mbean" "mbean name"
+                   :id :bean
+                   :parse-fn #(str %)]
+                  ["-o" "--operation setMaintenance" "operation name"
+                   :id :operation
+                   :parse-fn #(keyword %)]])
 
-(defn -main
-  [& args]
-  (println "Hello, World!"))
+(defn get-strings [[bean-name & bean-ops]]
+  (->> bean-ops
+       (flatten)
+       (map (partial str (.toString bean-name) " "))))
 
-(defn get-connection []
-  (.getMBeanServerConnection (JMXConnectorFactory/connect jmx-url)))
-
-(defn list-mbeans []
-  (let [pairs (->> (.queryNames (get-connection) object-name-pattern nil)
-                   (map #(->> (.getMBeanInfo connection %)
-                              (.getOperations)
-                              (map .getName)
-                              (map (partial str (.getCanoncialName %) " "))))
-                   (flatten))]
-    (doseq [line pairs] (println line))
+(defn list-beans []
+  (let [bean-strings (->> (jmx/mbean-names object-name-pattern)
+                          (sort)
+                          (map #(list % (jmx/operation-names %)))
+                          (map get-strings)
+                          (flatten))]
+    (doseq [mbean (flatten bean-strings)] (println mbean))
     (System/exit 0)))
 
+(defn- get-operation-info [bean-name operation]
+  (first (filter #(= (-> % .getName keyword) operation) (jmx/operations bean-name))))
+
+(defn get-params [bean-name operation]
+  (let [parameter-maps (->> (get-operation-info bean-name operation)
+                            (.getSignature)
+                            (map #(hash-map :name (.getName %)
+                                            :descr (.getDescription %)
+                                            :type (.getType %))))]
+    (doseq [parameter parameter-maps] (println parameter)))
+  (System/exit 0))
+
+(defn- cast-type [[arg type-name]]
+  (match [type-name]
+         [:int] (Integer. arg)
+         [:long] (Long. arg)
+         [:boolean] (Boolean. arg)
+         [:java.lang.String] arg))
+
+(defn invoke [bean-name operation]
+  (let [signature (.getSignature (get-operation-info bean-name operation))]
+    (if (> (count signature) 0)
+      (let [args (string/split (read-line) #" ")
+            typed-args (->> signature
+                            (map #(keyword (.getType %)))
+                            (map list args)
+                            (map cast-type))]
+        (apply jmx/invoke bean-name operation typed-args))
+      (println (jmx/invoke bean-name operation)))
+    (System/exit 0)))
+
+(defn describe [bean-name operation]
+  (println (.getDescription (get-operation-info bean-name operation))))
+
+(defn -main [& args]
+  (jmx/with-connection {:host "localhost" :port 11080}
+    (let [{:keys (options summary)} (cli/parse-opts args cli-options)]
+      (match [(:mode options)]
+             [:list] (list-beans)
+             [:params] (get-params (:bean options) (:operation options))
+             [:invoke] (invoke (:bean options) (:operation options))
+             [:describe] (describe (:bean options) (:operation options))
+             [:else] (print summary)))))
