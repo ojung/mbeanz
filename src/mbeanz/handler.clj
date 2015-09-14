8d8f5c80f7484fba9137f34e3a9bf9f178cc818d
Oskar Jung <oskar@mbr-targeting.com>
2015-09-14 15:10:30 +1200

    Unify describe and parameters route into describe

--- src/mbeanz/handler.clj
+++ src/mbeanz/handler.clj
@@ -18,11 +18,13 @@
 (defn- identifier-string [identifiers]
   (map #(str (:bean %) " " (stringify (:operation %))) identifiers))
 
-(defn- with-bean-and-operation [operation function]
+(defn- handle-describe [operation]
   (fn [request]
     (jmx/with-connection {:host jmx-remote-host :port jmx-remote-port}
-      (let [mbean (get-in request [:params :bean])]
-        (doall (function mbean (keyword operation)))))))
+      (let [mbean (get-in request [:params :bean])
+            op (keyword operation)]
+        {:body (hash-map :description (doall (describe mbean op))
+                         :parameters (doall (get-params mbean op)))}))))
 
 (defn- handle-invoke [operation]
   (fn [request]
@@ -30,8 +32,8 @@
       (let [mbean (get-in request [:params :bean])
             args (get-in request [:params :args])]
         (if (string? args)
-          (hash-map :body {:result (invoke mbean (keyword operation) args)})
-          (hash-map :body {:result (apply invoke mbean (keyword operation) args)}))))))
+          {:body {:result (invoke mbean (keyword operation) args)}}
+          {:body {:result (apply invoke mbean (keyword operation) args)}})))))
 
 (defn- handle-list-beans []
   (fn [request]
@@ -40,8 +42,7 @@
 
 (defroutes app-routes
   (GET "/list" [] (handle-list-beans))
-  (GET "/describe/:operation" [operation] (with-bean-and-operation operation describe))
-  (GET "/parameters/:operation" [operation] (with-bean-and-operation operation get-params))
+  (GET "/describe/:operation" [operation] (handle-describe operation))
   (GET "/invoke/:operation" [operation] (handle-invoke operation))
   (route/not-found "Not Found"))
 
