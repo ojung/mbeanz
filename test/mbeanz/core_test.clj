de07677aabe51b8a9803d6eec362282b82d569c8
Oskar Jung <oskar@mbr-targeting.com>
2015-10-01 09:36:29 +1300

    Update tests

--- test/mbeanz/core_test.clj
+++ test/mbeanz/core_test.clj
@@ -1,10 +1,9 @@
 (ns mbeanz.core-test
   (:require [clojure.test :refer :all]
-            [mbeanz.core :refer :all]
-            [mbeanz.common :refer :all])
+            [mbeanz.core :refer :all])
   (:import [java.lang.IllegalArgumentException]))
 
-(deftest index
+(deftest test-list-beans
   (testing "list single mbean"
     (is (= (list-beans "java.lang:type=Memory") [{:bean "java.lang:type=Memory" :operation :gc}])))
   (testing "list multiple mbeans"
@@ -18,43 +17,53 @@
             {:bean "java.lang:type=MemoryPool,name=PS Survivor Space"
              :operation :resetPeakUsage}]))))
 
-(deftest show
-  (testing "mbean description"
-    (is (= (describe "java.lang:type=Memory" :gc) "gc")))
-  (testing "mbean parameters"
-    (is (= (get-params "java.lang:type=Threading" :dumpAllThreads)
-           (list {:name "p0", :type "boolean", :description "p0"}
-                 {:name "p1", :type "boolean", :description "p1"})))))
+(deftest test-describe
+  (testing "mbean operation without arguments"
+    (is (= (describe "java.lang:type=Memory" :gc)
+           [{:name "gc", :description "gc", :signature []}])))
+  (testing "mbean operation with parameters"
+    (is (= (describe "java.lang:type=Threading" :dumpAllThreads)
+           [{:name "dumpAllThreads",
+             :description "dumpAllThreads",
+             :signature [{:description "p0", :name "p0", :type "boolean"}
+                         {:description "p1", :name "p1", :type "boolean"}]}]))))
 
 (deftest test-invoke
-  (testing "type deduction for monadic operations"
-    (is (= (get-typed-args "java.util.logging:type=Logging" :getLoggerLevel "asd")
-           [[:java.lang.String "asd"]])))
-  (testing "type deduction for variadic operations"
-    (is (= (get-typed-args "java.lang:type=Threading" :dumpAllThreads "true" "true")
-           [[:boolean "true"] [:boolean "true"]])))
   (testing "mbean with arguments"
-    (is (not (invoke "java.util.logging:type=Logging" :getLoggerLevel "asdfg"))))
-  ;TODO: handle overloaded signatures
-  ;(testing "overloaded mbean with arguments"
-    ;(is (invoke "java.lang:type=Threading" :getThreadUserTime "1")))
+    (is (not (invoke "java.util.logging:type=Logging"
+                     :getLoggerLevel
+                     ["java.lang.String" "someInexistentLogger"]))))
+  (testing "overloaded mbean with arguments"
+    (is (invoke "java.lang:type=Threading" :getThreadUserTime ["long" "1"])))
   (testing "mbean without arguments"
-    (is (= (invoke "java.lang:type=Memory" :gc) nil))))
+    (is (not (invoke "java.lang:type=Memory" :gc)))))
 
 (deftest type-casting
-  (testing "stringify function"
-    (is (= (stringify :int) "int"))
-    (is (= (stringify :Boolean) "Boolean")))
   (testing "int"
-    (is (= (cast-type [:int "1"]) (int 1))))
+    (is (= (cast-type ["int" "1"]) (int 1))))
+  (testing "Integer"
+    (is (= (cast-type ["java.lang.Integer" "1"]) (Integer. 1))))
   (testing "long"
-    (is (= (cast-type [:long "1"]) (long 1))))
+    (is (= (cast-type ["long" "1"]) 1)))
+  (testing "Long"
+    (is (= (cast-type ["java.lang.Long" "1"]) 1)))
   (testing "boolean"
-    (is (= (cast-type [:boolean "true"]) (boolean true))))
+    (is (= (cast-type ["boolean" "true"]) true)))
+  (testing "Boolean"
+    (is (= (cast-type ["java.lang.Boolean" "true"]) true)))
+  (testing "double"
+    (is (= (cast-type ["double" "1"]) (double 1))))
+  (testing "Double"
+    (is (= (cast-type ["java.lang.Double" "1"]) (double 1))))
+  (testing "float"
+    (is (= (cast-type ["float" "1"]) (float 1))))
+  (testing "Float"
+    (is (= (cast-type ["java.lang.Float" "1"]) (float 1))))
   (testing "string"
-    (is (= (cast-type [:java.lang.String "hello world"]) "hello world")))
+    (is (= (cast-type ["java.lang.String" "hello world"]) "hello world")))
+  (testing "number format exception"
+    (is (thrown? NumberFormatException (cast-type ["int" "asd"]))))
   (testing "unsupported type"
     (is (thrown-with-msg? IllegalArgumentException
                           #"Unsupported argument type"
-                          (cast-type '(:asdjl "asd"))))))
-
+                          (cast-type ["short" "asd"])))))
