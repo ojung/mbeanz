fd9741e1abfbfa4c25d3351a69392e56dbfc8136
Oskar Jung <oskar@mbr-targeting.com>
2015-10-14 12:34:30 +1300

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mbeanz "1.0.4-SNAPSHOT"
+(defproject mbeanz "1.1.1-SNAPSHOT"
   :description "helper to find and use mbeans"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
